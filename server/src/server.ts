ab76039126823219b26f6a4efb8b35e3e5e7c063
Shikhar Mishra <shikhatmishra730@gmail.com>
2022-10-19 18:11:40 +0530

    add helper middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './database/connectDB.js';
 dotenv.config();
-
+import connectDB from './database/connectDB.js';
+import cors from 'cors';
+import 'express-async-errors';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('MERNLY API');
