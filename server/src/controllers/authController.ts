c828cc8ae1da73866ea7bf2dfcc4ecfcf2b18619
Shikhar Mishra <shikhatmishra730@gmail.com>
2022-10-20 22:18:59 +0530

    create signup controller

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,7 +1,31 @@
 import { Request, Response } from 'express';
+import { BadRequestError } from '../errors/errors.js';
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/User.js';
 
 const signup = async (req: Request, res: Response) => {
-  res.send('signup');
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    throw new BadRequestError('Please provide all the values');
+  }
+  const validUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (validUser) {
+    throw new BadRequestError('Email or username already in use');
+  }
+  const user = new User({ username, email, password });
+
+  const accessToken = user.createAccessToken();
+  const refreshToken = user.createRefreshToken();
+
+  user.refreshTokens.push(refreshToken);
+  await user.save();
+
+  console.log(accessToken, refreshToken);
+  res.status(StatusCodes.OK).send({
+    user,
+    accessToken,
+    refreshToken,
+  });
 };
 
 const signin = async (req: Request, res: Response) => {
