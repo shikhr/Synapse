47c2a9b114dfc55fd60197f0e9bb8e13aebb83a8
Shikhar Mishra <shikharmishra730@gmail.com>
2022-12-27 20:50:03 +0530

    change modal to use react portal

--- client/src/components/UI/Modal.tsx
+++ client/src/components/UI/Modal.tsx
@@ -1,14 +1,15 @@
 import { ReactElement } from 'react';
-
+import { createPortal } from 'react-dom';
 interface ModalProps {
   children: ReactElement;
 }
 
 const Modal = ({ children }: ModalProps) => {
-  return (
-    <div className="w-full h-full z-50 md:max-w-2xl md:h-auto md:max-h-[90vh] md:rounded-md overscroll-none overflow-auto bg-background-dark fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+  return createPortal(
+    <div className="w-full h-full z-modal md:max-w-2xl md:h-auto md:max-h-[90vh] md:rounded-md overscroll-none overflow-auto bg-background-dark fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       {children}
-    </div>
+    </div>,
+    document.getElementById('overlay-root') as HTMLDivElement
   );
 };
 
--- client/src/components/UI/Overlay.tsx
+++ client/src/components/UI/Overlay.tsx
@@ -1,12 +1,15 @@
+import { createPortal } from 'react-dom';
+
 interface OverlayProps {
   closeModal: () => void;
 }
 const Overlay = ({ closeModal }: OverlayProps) => {
-  return (
+  return createPortal(
     <div
       onClick={closeModal}
-      className="fixed z-40 top-0 left-0 bottom-0 right-0 bg-white bg-opacity-20"
-    ></div>
+      className="fixed z-overlay top-0 left-0 bottom-0 right-0 bg-white bg-opacity-20"
+    ></div>,
+    document.getElementById('overlay-root') as HTMLDivElement
   );
 };
 export default Overlay;
