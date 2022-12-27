a210ae82ac9ca7f8ffba1c4dd1156d5da104c5c3
Shikhar Mishra <shikharmishra730@gmail.com>
2022-12-27 20:51:23 +0530

    create custom hook for popups

---
+++ client/src/hooks/useOuterClick.ts
@@ -0,0 +1,29 @@
+import { useCallback, useRef, useState } from 'react';
+
+const useOuterClick = <T extends HTMLElement>() => {
+  const [isComponentVisible, setIsComponentVisible] = useState(false);
+  const ref = useRef<T | null>(null);
+
+  const _onClick = (e: MouseEvent): void => {
+    e.preventDefault();
+    e.stopPropagation();
+    const clickedOutside = !ref.current?.contains(e.target as Node);
+    if (clickedOutside) setIsComponentVisible(false);
+  };
+
+  const innerRef = useCallback((node: T) => {
+    if (ref.current) {
+      console.log('removing');
+      document.removeEventListener('click', _onClick, true);
+    }
+    if (node !== null) {
+      console.log('adding addEventListener');
+      document.addEventListener('click', _onClick, true);
+    }
+    ref.current = node;
+  }, []);
+
+  return { innerRef, isComponentVisible, setIsComponentVisible };
+};
+
+export default useOuterClick;
