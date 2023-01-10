356ba336a81c5d8ed4b12691f0cd82976d369512
Shikhar Mishra <shikharmishra730@gmail.com>
2023-01-10 21:16:15 +0530

    fix modal rendering in the wrong root

--- client/src/components/UI/Modal.tsx
+++ client/src/components/UI/Modal.tsx
@@ -9,7 +9,7 @@ const Modal = ({ children }: ModalProps) => {
     <div className="w-full h-full z-modal md:max-w-2xl md:h-auto md:max-h-[90vh] md:rounded-md overscroll-none overflow-auto bg-background-dark fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       {children}
     </div>,
-    document.getElementById('overlay-root') as HTMLDivElement
+    document.getElementById('modal-root') as HTMLDivElement
   );
 };
 
