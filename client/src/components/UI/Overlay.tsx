ee6f95a88b15c91aa04c46fb6875481afd510aaa
Shikhar Mishra <shikharmishra730@gmail.com>
2023-01-10 23:34:53 +0530

    add class props to overlay

--- client/src/components/UI/Overlay.tsx
+++ client/src/components/UI/Overlay.tsx
@@ -2,12 +2,13 @@ import { createPortal } from 'react-dom';
 
 interface OverlayProps {
   closeModal: () => void;
+  className?: string;
 }
-const Overlay = ({ closeModal }: OverlayProps) => {
+const Overlay = ({ closeModal, className }: OverlayProps) => {
   return createPortal(
     <div
       onClick={closeModal}
-      className="fixed z-overlay top-0 left-0 bottom-0 right-0 bg-white bg-opacity-20"
+      className={`fixed z-overlay top-0 left-0 bottom-0 right-0 bg-white bg-opacity-20 ${className}`}
     ></div>,
     document.getElementById('overlay-root') as HTMLDivElement
   );
