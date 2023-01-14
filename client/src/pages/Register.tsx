ca448fc71f94f1e285008227364e73b7adff0c94
Shikhar Mishra <shikharmishra730@gmail.com>
2023-01-14 15:48:27 +0530

    chnage name and icons for site

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,7 +4,7 @@ import RegisterForm from '../components/Forms/RegisterForm';
 const Register = () => {
   return (
     <div className="bg-background-dark flex flex-col items-center min-h-screen">
-      <div className="pt-4 w-48 sm:w-60">
+      <div className="py-4 sm:flex sm:flex-1 max-h-40 justify-center items-center w-48 sm:w-60">
         <img src={logo} alt="logo" />
       </div>
       <div className="flex-1 w-full flex sm:items-center sm:justify-center">
